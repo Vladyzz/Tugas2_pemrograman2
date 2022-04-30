7e04c6e3490f46c47d792c7f5a5ee35212464e62
adityabintang18 <adityabintangpradana@gmail.com>
2022-04-30 11:26:11 +0700

    Initial commit

---
+++ src/soal_1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class soal_1 {
+
+    public static void main(String args[]){
+
+        int input,  i;
+        Scanner angka = new Scanner(System.in);
+
+        System.out.println("Nama : Aditya Bintang Pradana");
+        System.out.println("NPM :  202143501613");
+        System.out.println("Soal 1");
+        System.out.println("==============================");
+
+        System.out.print("Input Angka : ");
+        input = angka.nextInt();
+        System.out.print("Bilangan yang merupakan kelipatan 3 atau 5 adalah ");
+
+        i = 1;
+        do {
+            if (i % 3 == 0 || i % 5 == 0)  {
+                System.out.print( i );
+                System.out.print(" ");
+            }
+            i ++;
+        } while (i <= input);
+
+    }
+}
